86697e81eb581fe1bbd0853ad2e9483bc5d69ba5
LautiiGomez <lautigomezsalvi@gmail.com>
2025-04-22 12:10:47 -0300

    Paso 1: Configuración inicial

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,14 @@
+// Primer consigna: Configuración inicial
+// Autor: Lautaro Gómez Salvi
+// Fecha: 2025-04-22
+
+fun main() {
+    println("¡Hola, Kotlin!")
+}
+
+fun alumno() {
+    println("Lautaro Gómez Salvi")
+    println("2025-04-22")
+}
+
+// Segunda consigna: Variables y Tipos de Datos
