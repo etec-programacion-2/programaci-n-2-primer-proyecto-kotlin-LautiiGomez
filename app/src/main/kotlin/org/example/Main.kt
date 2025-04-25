81424fa256a45468cfa1c5baed626f275557ad96
LautiiGomez <lautigomezsalvi@gmail.com>
2025-04-25 10:35:34 -0300

    Paso 4: Loops

--- app/src/main/kotlin/org/example/Main.kt
+++ app/src/main/kotlin/org/example/Main.kt
@@ -41,13 +41,11 @@ fun numeros() {
 }
 
 // Tercera consigna: Control de flujo
+// Para usar el control de flujo, usamos las palabras reservadas if, else, when, try, catch, finally.
 
 fun esPar(numero: Int): Boolean {
     return numero % 2 == 0
 }
-
-// Función alternativa con expresión de una línea
-fun esParSimple(numero: Int) = numero % 2 == 0
 
 fun ParOImpar() {
     val numero = 42
@@ -58,6 +56,91 @@ fun ParOImpar() {
         println("$numero es un número impar")
     }
     
-    // También puedes usar la función directamente en una expresión
     println("17 es ${if (esPar(17)) "par" else "impar"}")
+}
+
+// Cuarta consigna: Loops
+// Para usar los loops, usamos las palabras reservadas while, do, for, in, break, continue.
+
+fun mostrarNumerosCon_For() {
+    println("Números del 1 al 10 usando bucle FOR:")
+    for (numero in 1..10) {
+        println(numero)
+    }
+}
+
+fun mostrarNumerosCon_While() {
+    println("Números del 1 al 10 usando bucle WHILE:")
+    var numero = 1
+    while (numero <= 10) {
+        println(numero)
+        numero++
+    }
+}
+
+fun mostrarNumerosCon_DoWhile() {
+    println("Números del 1 al 10 usando bucle DO-WHILE:")
+    var numero = 1
+    do {
+        println(numero)
+        numero++
+    } while (numero <= 10)
+}
+
+fun paresConFor() {
+    for (n_p in 2..20 step 2) {
+        println(n_p)
+    }
+}
+
+fun paresConWhile() {
+    var n_p = 2
+    while (i <= 20) {
+        println(i)
+        n_p += 2
+    }
+}
+
+fun paresConDoWhile() {
+    var n_p = 2
+    do {
+        println(n_p)
+        n_p += 2
+    } while (n_p <= 20)
+}
+
+
+fun imprimir(){
+    main()
+    println()
+
+    alumno()
+    println()
+
+    VariablesyTiposDeDatos()
+    println()
+
+    numeros()
+    println()
+
+    ParOImpar()
+    println()
+
+    mostrarNumerosCon_For()
+    println()
+
+    mostrarNumerosCon_While()
+    println()
+
+    mostrarNumerosCon_DoWhile()
+    println()
+
+    paresConFor()
+    println()
+
+    paresConWhile()
+    println()
+
+    paresConDoWhile()
+    println()
 }
